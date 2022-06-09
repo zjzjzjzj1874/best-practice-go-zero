406d2a2c4eeaf3f0b611425524212df7a6e02d73
zjzzzjjj <zjzjzjzj1874@gmail.com>
2022-06-09 16:34:22 +0800

    feat✨:($chore): add missing file
    Signed-off-by: zjzzzjjj <zjzjzjzj1874@gmail.com>

---
+++ my_zero/internal/logic/migrateLogic.go
@@ -0,0 +1,28 @@
+package logic
+
+import (
+	"context"
+	"github.com/zjzjzjzj1874/best-pracrice-go-zero/my_zero/internal/svc"
+	"github.com/zjzjzjzj1874/best-pracrice-go-zero/my_zero/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type MigrateLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewMigrateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *MigrateLogic {
+	return &MigrateLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+// Migrate 自动化生产表
+func (l *MigrateLogic) Migrate(req *types.MigrateRequest) (resp *types.MigrateResponse, err error) {
+	return nil, l.svcCtx.MysqlDB.MigrateWithApi()
+}
