360ceda7e95b7f39645d746b002489567712e3a2
zjzzzjjj <zjzjzjzj1874@gmail.com>
2022-08-03 14:54:53 +0800

    docs📝(mid): add some note
    Signed-off-by: zjzzzjjj <zjzjzjzj1874@gmail.com>

--- my_zero/my-zero.go
+++ my_zero/my-zero.go
@@ -27,6 +27,7 @@ func main() {
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
 
+	// todo rabbitmq学习pprof的开启和zero中prometheus的开启,将从这里取消哦,以后添加一个中间件初始化的方法,初始化pprof,mq,cron的任务等等
 	helper.OpenPPROF(c.PprofConf)
 	cron.Init(ctx) // 初始化轮询任务
 	//rabbitmq.InitProducer(context.TODO(), ctx.Config.RabbitMQ) // 初始化消息队列生产者
