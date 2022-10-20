4ac5e46b20b527bc8a2e4851c2b47a1dd63033da
zjzzzjjj <zjzjzjzj1874@gmail.com>
2022-10-20 15:28:38 +0800

    feat✨(zero): add singleflight
    Signed-off-by: zjzzzjjj <zjzjzjzj1874@gmail.com>

--- my_zero/internal/logic/liveness_logic.go
+++ my_zero/internal/logic/liveness_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"golang.org/x/sync/singleflight"
 
 	"github.com/zjzjzjzj1874/best-pracrice-go-zero/my_zero/internal/svc"
 	"github.com/zjzjzjzj1874/best-pracrice-go-zero/my_zero/internal/types"
@@ -23,8 +24,18 @@ func NewLivenessLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Liveness
 	}
 }
 
+// Liveness singleflight => singleflight的原理是当同时有很多请求同时到来时，最终只有一个请求会最终访问到资源，
+// 其他请求都会等待结果然后返回 => https://www.cyningsun.com/01-11-2021/golang-concurrency-singleflight.html
+// 以及singleFlight的一些缺点:https://juejin.cn/post/7093859835694809125
 func (l *LivenessLogic) Liveness(req *types.LivenessRequest) (resp *types.LivenessResponse, err error) {
-	// todo: add your logic here and delete this line
+	sf := singleflight.Group{}
+	v, err, _ := sf.Do("", func() (interface{}, error) {
 
-	return
+		return resp, nil
+	})
+	if err != nil {
+		logx.Errorf("do failure:[err:%s]", err.Error())
+		return nil, err
+	}
+	return v.(*types.LivenessResponse), nil
 }
