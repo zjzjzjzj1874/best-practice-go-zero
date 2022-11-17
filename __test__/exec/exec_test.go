83a224d31a18ccf376e334740be536b261272ddb
zjzzzjjj <zjzjzjzj1874@gmail.com>
2022-11-17 10:16:26 +0800

    test✅(cmd): add downloading process
    Signed-off-by: zjzzzjjj <zjzjzjzj1874@gmail.com>

--- __test__/exec/exec_test.go
+++ __test__/exec/exec_test.go
@@ -2,9 +2,12 @@ package exec
 
 import (
 	"bytes"
+	"io"
+	"io/ioutil"
 	"log"
 	"os/exec"
 	"testing"
+	"time"
 )
 
 // detail see this article: https://colobu.com/2020/12/27/go-with-os-exec/
@@ -18,6 +21,31 @@ func TestCMD(t *testing.T) {
 			log.Fatalf("failed to call cmd.Run(): %v", err)
 		}
 		t.Logf(out.String())
+	})
+
+	t.Run("#show downloading process", func(t *testing.T) {
+		cmd := exec.Command("curl", "https://dl.google.com/go/go1.15.6.linux-amd64.tar.gz")
+		var stdoutProcessStatus bytes.Buffer
+		cmd.Stdout = io.MultiWriter(ioutil.Discard, &stdoutProcessStatus)
+		done := make(chan struct{})
+		go func() {
+			tick := time.NewTicker(time.Second)
+			defer tick.Stop()
+			for {
+				select {
+				case <-done:
+					return
+				case <-tick.C:
+					log.Printf("downloaded: %d KB", stdoutProcessStatus.Len()/1024)
+					log.Printf("downloaded: %d MB", stdoutProcessStatus.Len()/1024/1024)
+				}
+			}
+		}()
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalf("failed to call Run(): %v", err)
+		}
+		close(done)
 	})
 
 }
