e6a8f2174d75a101b8585ea336e8ba4470b83072
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2023-03-29 17:20:58 +0800

    feat✨(cors): customer with response header
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- task/task.go
+++ task/task.go
@@ -4,7 +4,6 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
-
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest"
@@ -15,6 +14,7 @@ import (
 	"github.com/zjzjzjzj1874/best-pracrice-go-zero/task/internal/cron"
 	"github.com/zjzjzjzj1874/best-pracrice-go-zero/task/internal/handler"
 	"github.com/zjzjzjzj1874/best-pracrice-go-zero/task/internal/svc"
+	"net/http"
 )
 
 var configFile = flag.String("f", "etc/task.yaml", "the config file")
@@ -29,7 +29,8 @@ func main() {
 	c.Swagger = swagger
 
 	ctx := svc.NewServiceContext(c)
-	server := rest.MustNewServer(c.RestConf, rest.WithCors())
+
+	server := rest.MustNewServer(c.RestConf, Opt)
 	defer server.Stop()
 
 	httpx.SetErrorHandler(errors.ErrorRestfulHandler) // 自定义错误返回,restful类型
@@ -44,3 +45,12 @@ func main() {
 
 //go:embed swagger.json
 var swagger []byte
+
+// Opt 设置自定义跨域请求,或者返回的header
+var Opt = rest.WithCustomCors(func(header http.Header) {
+	header.Set("Access-Control-Allow-Headers", "*")
+	header.Set("Access-Control-Allow-Origin", "*")
+	header.Set("Access-Control-Expose-Headers", "*")
+	header.Set("Access-Control-Allow-Methods", "*")
+	header.Set("Access-Control-Allow-Credentials", "false")
+}, nil, "*")
