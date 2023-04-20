f53287532d10aea5aac309ec8960f354e68fc455
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2023-04-20 14:20:23 +0800

    docs📝(json): update doc info
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- __test__/json/main_test.go
+++ __test__/json/main_test.go
@@ -4,34 +4,24 @@ import (
 	"testing"
 )
 
-//func TestMarshalWithJson(t *testing.T) {
-//	MarshalWithJson()
-//}
-//
-//func TestMarshalWithJsonEncodeAndBuf(t *testing.T) {
-//	MarshalWithJsonEncodeAndBuf()
-//}
+func TestMarshalWithJson(t *testing.T) {
+	MarshalWithJson()
+}
+
+func TestMarshalWithJsonEncodeAndBuf(t *testing.T) {
+	MarshalWithJsonEncodeAndBuf()
+}
 
 func BenchmarkMarshalWithJson(b *testing.B) {
-	//var memStats runtime.MemStats
-	//runtime.ReadMemStats(&memStats)
 	for i := 0; i < b.N; i++ {
 		MarshalWithJson()
 	}
-	//runtime.ReadMemStats(&memStats)
-	//b.Logf("Alloc: %d bytes, TotalAlloc: %d bytes, Sys: %d bytes, NumGC: %d",
-	//	memStats.Alloc, memStats.TotalAlloc, memStats.Sys, memStats.NumGC)
 }
 
 func BenchmarkMarshalWithJsonEncodeAndBuf(b *testing.B) {
-	//var memStats runtime.MemStats
-	//runtime.ReadMemStats(&memStats)
 	for i := 0; i < b.N; i++ {
 		MarshalWithJsonEncodeAndBuf()
 	}
-	//runtime.ReadMemStats(&memStats)
-	//b.Logf("Alloc: %d bytes, TotalAlloc: %d bytes, Sys: %d bytes, NumGC: %d",
-	//	memStats.Alloc, memStats.TotalAlloc, memStats.Sys, memStats.NumGC)
 }
 
 // 基于内存的性能测试:go test -bench='Marshal' . -benchmem
